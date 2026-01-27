da854b18953035e72bbe52298267d32350664c6e
Riley Martin <riley.na.martin@gmail.com>
2026-01-27 05:22:07 +0000

    Add RSS staging

--- dbt/models/staging/stg_raw_rss.sql
+++ dbt/models/staging/stg_raw_rss.sql
@@ -1,7 +1,19 @@
-SELECT
+select
     source as feed_name,
     json_value(raw_content, '$.title') as title,
     json_value(raw_content, '$.summary') as summary,
-    json_value(raw_content, '$.published') as published_at,
+    json_value(raw_content, '$.link') as link,
+    timestamp(datetime(
+        cast(json_value(raw_content, '$.published_parsed[0]') as int64),
+        cast(json_value(raw_content, '$.published_parsed[1]') as int64),
+        cast(json_value(raw_content, '$.published_parsed[2]') as int64),
+        cast(json_value(raw_content, '$.published_parsed[3]') as int64),
+        cast(json_value(raw_content, '$.published_parsed[4]') as int64),
+        cast(json_value(raw_content, '$.published_parsed[5]') as int64)
+    )) as published_at,
     ingested_at
-FROM {{ source('raw_rss_data', 'rss_feeds_raw') }}
+from {{ source('raw_rss_data', 'rss_feeds_raw')}}
+qualify row_number() over (
+    partition by link
+    order by published_at desc
+) = 1
