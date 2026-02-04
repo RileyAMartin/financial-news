9f5fafe966283748667170ab1905fddd91ccf9ec
Riley Martin <riley.na.martin@gmail.com>
2026-02-04 07:59:36 +0000

    Add IMF country abbreviations seed and filter for valid country codes in the int_imf_qnea_filtered model

--- dbt/models/intermediate/int_imf_qnea_filtered.sql
+++ dbt/models/intermediate/int_imf_qnea_filtered.sql
@@ -6,6 +6,11 @@ with qnea_indicators as (
 qnea_data as (
     select *
     from {{ ref("stg_imf_qnea") }}
+),
+
+imf_countries as (
+    select country_code
+    from {{ ref("imf_country_abbreviations") }}
 )
 
 select
@@ -20,3 +25,6 @@ from qnea_data
 
 inner join qnea_indicators
 on qnea_data.indicator_code = qnea_indicators.indicator_code
+
+inner join imf_countries
+on qnea_data.country_code = imf_countries.country_code
--- dbt/models/intermediate/int_imf_qnea_with_fx.sql
+++ dbt/models/intermediate/int_imf_qnea_with_fx.sql
@@ -25,7 +25,7 @@ select
     (qnea_data.obs_value * qnea_data.annualization_multiplier * fx_data.value_eur) as value_eur
 from qnea_data
 
-inner join fx_data
+left join fx_data
 on
     qnea_data.country_code = fx_data.country_code
     and qnea_data.period_end_date = fx_data.period_end_date
