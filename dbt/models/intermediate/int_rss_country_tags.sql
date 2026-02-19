3d5ac2a13a1abffd17c115becfbb291b73e202c2
Riley Martin <riley.na.martin@gmail.com>
2026-02-19 11:03:40 +0000

    Add selectors and make news models incremental

--- dbt/models/intermediate/int_rss_country_tags.sql
+++ dbt/models/intermediate/int_rss_country_tags.sql
@@ -1,10 +1,22 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='link'
+    )
+}}
+
 with news as (
     select
         link,
+        published_at,
         lower(
             regexp_replace(concat(title, " ", coalesce(summary, "")), r"[^a-zA-Z\s]", "")
         ) as full_text
     from {{ ref("stg_rss") }}
+
+    {% if is_incremental() %}
+    where published_at > (select max(published_at) from {{ this }})
+    {% endif %}
 ),
 
 keywords as (
@@ -17,6 +29,7 @@ keywords as (
 matched_tags as (
     select
         news.link,
+        news.published_at,
         keywords.country_code
     from news
     inner join keywords
@@ -28,6 +41,7 @@ matched_tags as (
 
 select
     link,
+    published_at,
     array_agg(distinct country_code ignore nulls) as country_codes
 from matched_tags
-group by 1
+group by 1, 2
